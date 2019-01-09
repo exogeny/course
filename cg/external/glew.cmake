ee0890562a44be4cd39c0072bfc32382482b9a97
Terence Wang <terence@culaer.com>
2019-01-09 13:17:27 +0800

    Solve the compile error for Windows x64 and Win32

--- cg/external/glew.cmake
+++ cg/external/glew.cmake
@@ -5,33 +5,44 @@ set(glew_BUILD        ${CMAKE_CURRENT_BINARY_DIR}/glew/src/glew)
 set(glew_INSTALL      ${CMAKE_CURRENT_BINARY_DIR}/glew/install)
 
 if(WIN32)
-  if(${CMAKE_GENERATOR} MATCHES "Visual Studio.*")
-    set(glew_STATIC_LIBRARIES
-        debug ${glew_INSTALL}/lib/glew${BUILD_ARCH}d.lib
-        optimized ${glew_INSTALL}/lib/glew${BUILD_ARCH}.lib)
-  else()
-    if(CMAKE_BUILD_TYPE EQUAL Debug)
-      set(glew_STATIC_LIBRARIES ${glew_INSTALL}/lib/glew${BUILD_ARCH}d.lib)
-    else()
-      set(glew_STATIC_LIBRARIES ${glew_INSTALL}/lib/glew${BUILD_ARCH}.lib)
-    endif()
-  endif()
-  set(glew_LIBRARIES ${glew_INSTALL}/bin/glew${BUILD_ARCH}$<$<CONFIG:Debug>:d>.dll)
+  set(glew_STATIC_LIBRARIES ${glew_INSTALL}/lib/glew32.lib)
+  set(glew_LIBRARIES ${glew_INSTALL}/bin/glew32.dll)
 else()
   set(glew_LIBRARIES ${glew_INSTALL}/lib${BUILD_ARCH}/libGLEW.so.2.1.0)
-  set(glew_STATIC_LIBRARIES ${glew_INSTALL}/lib${BUILD_ARCH}/libGLEW$<$<CONFIG:Debug>:d>.a)
+  set(glew_STATIC_LIBRARIES ${glew_INSTALL}/lib${BUILD_ARCH}/libGLEW.a)
 endif()
 set(glew_INCLUDE_DIRS ${glew_INSTALL}/include)
 
-ExternalProject_Add(glew
-  PREFIX            glew
-  URL               ${glew_URL}
-  URL_HASH          MD5=dff2939fd404d054c1036cc0409d19f1
-  DOWNLOAD_DIR      "${DOWNLOAD_LOCATION}"
-  BUILD_IN_SOURCE   1
-  CONFIGURE_COMMAND ${CMAKE_COMMAND} ${CMAKE_CURRENT_BINARY_DIR}/glew/src/glew/build/cmake
-  PATCH_COMMAND     ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_CURRENT_SOURCE_DIR}/patches/glew/CMakeLists.txt ${glew_BUILD}/build/cmake
-  INSTALL_COMMAND   ${CMAKE_COMMAND}  -DCMAKE_INSTALL_PREFIX=${glew_INSTALL} 
-                                      -DCMAKE_BUILD_TYPE:STRING=Release
-                                      -P cmake_install.cmake
+if(WIN32)
+  if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+    set(Platform        x64)
+  else()
+    set(Platform        Win32)
+  endif()
+  ExternalProject_Add(glew
+    PREFIX            glew
+    URL               ${glew_URL}
+    URL_HASH          MD5=dff2939fd404d054c1036cc0409d19f1
+    DOWNLOAD_DIR      "${DOWNLOAD_LOCATION}"
+    BUILD_IN_SOURCE   1
+    CONFIGURE_COMMAND ${CMAKE_COMMAND} ${CMAKE_CURRENT_BINARY_DIR}/glew/src/glew/build/cmake -A ${Platform}
+    CMAKE_CACHE_ARGS  -DCMAKE_BUILD_TYPE:STRING=Release
+    BUILD_COMMAND     msbuild glew.sln /p:Configuration=Release /p:Platform=${Platform}
+    PATCH_COMMAND     ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_CURRENT_SOURCE_DIR}/patches/glew/CMakeLists.txt ${glew_BUILD}/build/cmake
+    INSTALL_COMMAND   ${CMAKE_COMMAND} -DCMAKE_INSTALL_PREFIX=${glew_INSTALL}
+                                       -P cmake_install.cmake
 )
+else()
+  ExternalProject_Add(glew
+    PREFIX            glew
+    URL               ${glew_URL}
+    URL_HASH          MD5=dff2939fd404d054c1036cc0409d19f1
+    DOWNLOAD_DIR      "${DOWNLOAD_LOCATION}"
+    BUILD_IN_SOURCE   1
+    CONFIGURE_COMMAND ${CMAKE_COMMAND} ${CMAKE_CURRENT_BINARY_DIR}/glew/src/glew/build/cmake
+    CMAKE_CACHE_ARGS  -DCMAKE_BUILD_TYPE:STRING=Release
+    PATCH_COMMAND     ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_CURRENT_SOURCE_DIR}/patches/glew/CMakeLists.txt ${glew_BUILD}/build/cmake
+    INSTALL_COMMAND   ${CMAKE_COMMAND} -DCMAKE_INSTALL_PREFIX=${glew_INSTALL}
+                                       -P cmake_install.cmake
+)
+endif()
